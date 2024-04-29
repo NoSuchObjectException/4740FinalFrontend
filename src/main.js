2dfed4bafe282832eb0f44ba30eca57acff79706
NoSuchObjectException <asriva8@lsu.edu>
2024-04-29 17:49:01 -0500

    updated import

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import "././app.pcss";
+import "src/app.pcss";
 import App from "././app.svelte";
 import App2 from "././app2.svelte";
 import App3 from "././secondPassUkrainePie.svelte"
 import App4 from "././secondPassRussiaPie.svelte"
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 const app = new App({
   target: document.getElementById("app"),
@@ -23,19 +23,19 @@ const app4 = new App4({
 });
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDN99C589zcw7bpLII2c5A65vPv3ob4MY0",
-  authDomain: "dashboard-e53f9.firebaseapp.com",
-  projectId: "dashboard-e53f9",
-  storageBucket: "dashboard-e53f9.appspot.com",
-  messagingSenderId: "158544210081",
-  appId: "1:158544210081:web:b0581c6a06aa96da3a28e0",
-  measurementId: "G-81TDN9HPFQ"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDN99C589zcw7bpLII2c5A65vPv3ob4MY0",
+//   authDomain: "dashboard-e53f9.firebaseapp.com",
+//   projectId: "dashboard-e53f9",
+//   storageBucket: "dashboard-e53f9.appspot.com",
+//   messagingSenderId: "158544210081",
+//   appId: "1:158544210081:web:b0581c6a06aa96da3a28e0",
+//   measurementId: "G-81TDN9HPFQ"
+// };
 
-// Initialize Firebase
-const app_fb = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app_fb);
+// // Initialize Firebase
+// const app_fb = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app_fb);
 
 
 export default app;
